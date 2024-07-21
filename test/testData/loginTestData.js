1fc988771fe26f458c3edb1d81dbb0ab3cd5ba74
elvsab <elvsab@bk.ru>
2024-07-21 21:16:56 +0300

    Update loginTestData.js
    changed test data for the 2nd test

--- test/testData/loginTestData.js
+++ test/testData/loginTestData.js
@@ -4,8 +4,8 @@ module.exports = {
       ['another_user', 'another_password', 'Epic sadface: Username is required']
     ],
     credentialsWithUsername: [
-      ['test_user', 'test_password', 'Epic sadface: Username is required'],
-      ['another_user', 'another_password', 'Epic sadface: Username is required']
+      ['test_user', 'test_password', 'Epic sadface: Password is required'],
+      ['another_user', 'another_password', 'Epic sadface: Password is required']
     ],
     validCredentials: [
       ['standard_user', 'secret_sauce']
